147b4e39fcbb32ed62d96ae6ae4c9a49f432a1a9
Alperen <alperensarisan@icloud.com>
2024-01-27 23:33:33 -0400

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Alperen Sarışan on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
